5f9e4d3f08a150c6e517001c4f95c7970342f942
rachmadaniHaryono <foreturiga@gmail.com>
2018-07-23 12:45:14 +0800

    new: dev: packaging files
    - manifest.in file is based on this
    https://github.com/ionelmc/cookiecutter-pylibrary/blob/master/%7B%7Bcookiecutter.repo_name%7D%7D/MANIFEST.in
    - setup.py is modified from @ulrichard
    https://github.com/HelloZeroNet/ZeroNet/issues/417#issuecomment-256174420
      - package is based on requirements.txt
      - entry point is not final and may changed later

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name='zeronet',
+    version='0.6.2',
+    author='HelloZeroNet',
+    author_email='',
+    description='Decentralized websites using BitCoin crypto and BitTorrent network',
+    url='https://github.com/HelloZeroNet/ZeroNet',
+    install_requires=[
+        'gevent>=1.1.0',
+        'msgpack>=0.4.4',
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=('src/Test', )),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+    ],
+    entry_points={
+        'console_scripts': [
+            'zeronet = zeronet:main',
+        ],
+    }
+)
